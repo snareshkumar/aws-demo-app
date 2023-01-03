23ceaac32fb03076862e2d78143696be7b48bbdc
nareshkumars <naresh.samesh@gmail.com>
2023-01-03 17:55:48 +0530

    SQS Properties and consumer logic added

--- src/main/java/com/spring/cloud/awsdemoapp/sqs/SqsDemo.java
+++ src/main/java/com/spring/cloud/awsdemoapp/sqs/SqsDemo.java
@@ -1,9 +1,12 @@
 package com.spring.cloud.awsdemoapp.sqs;
 
+import javax.management.RuntimeErrorException;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.MessagingException;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,9 +32,15 @@ public class SqsDemo {
         return "published successfully";
     }
 
-    @SqsListener("sqs-test")
-    public void consumeMessage(String message) {
-        log.info("consumed message from sqs is: " + message);
+    @SqsListener(value = "sqs-queue1", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void consumeMessage(String message) throws Exception {
+
+        if (message.equalsIgnoreCase("naresh")) {
+            log.info("Exception occurred {}", message);
+            throw new Exception("Intentionally throwing exception");
+        } else {
+            log.info("Message successfully consumed : {} ", message);
+        }
     }
 
 }
